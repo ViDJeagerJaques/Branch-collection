7bc2acdb0af0bdb09edcbfcc26526d3c861d0e10
ViD_Atha <ooddaangg@gmail.com>
2025-09-16 21:49:44 +0700

    File lx lama jir

---
+++ test.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    for(int i = 0; i < 20; i++) {
+        printf("SAYA JANJI TIDAK NGOCOK LAGI, JIKA SAYA NGOOCOK LAGI MAKA SAYA BERJANJILAGI\n");
+    }
+    return 0;
+}
